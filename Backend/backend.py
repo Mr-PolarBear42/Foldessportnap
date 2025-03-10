d84edcd75ec0458b3a86ad7c14a9f309750be560
Szorenyi Zalan Andras <szorenyi.za@gmail.com>
2025-03-10 18:31:50 +0100

    added sendData to most existing sports

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import gspread  # To interact with Google Sheets
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from flask_cors import CORS  
 
@@ -10,25 +10,28 @@ CORS(app)
 SCOPE = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 CREDS = ServiceAccountCredentials.from_json_keyfile_name("C:\\Users\\szori\\OneDrive\\Asztali gép\\Zalán mappa\\Coding\\Foldessportnap\\Backend\\credentials.json", SCOPE)
 client = gspread.authorize(CREDS)
-sheet = client.open("Sportnapteszt").sheet1  # Replace with your sheet name
+sheet = client.open("Sportnapteszt").sheet1 #Replace with sheets name
 
 @app.route('/api/data', methods=['POST'])
 def add_data():
-    new_data = request.json  # Get data from the frontend
-    print("Received data:", new_data)  # Log the received data
+    new_data = request.json 
+    #print("Received data:", new_data)
 
-    # Ensure all required fields are present and valid
+    
     required_fields = ['name', 'email', 'grade', 'class', 'sport', 'teammates', 'misc']
     for field in required_fields:
         if field not in new_data:
             return jsonify({"error": f"Missing required field: {field}"}), 400
 
-    # Convert teammates array to a string (if it's an array)
-    if isinstance(new_data['teammates'], list):
-        new_data['teammates'] = ', '.join(new_data['teammates'])
-
+    
+    new_data['teammates']=str(new_data['teammates'].values())
+    
+    new_data['teammates']=new_data['teammates'].removesuffix('])') 
+    new_data['teammates']=new_data['teammates'].removeprefix('dict_values([')
+    
+    
     try:
-        # Append the data to the sheet
+      
         sheet.append_row([
             new_data['name'],
             new_data['email'],
