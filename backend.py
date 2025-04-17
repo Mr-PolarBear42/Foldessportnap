674ea109ba592759ded664ffc1bb5262e60254a8
Szorenyi Zalan Andras <szorenyi.za@gmail.com>
2025-04-17 20:43:44 +0200

    almost nothing

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-
+import openpyxl
+from openpyxl import load_workbook
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -15,7 +16,20 @@ def receive_data():
     data = request.get_json()  # Get JSON data from request
     print("Received data:", data)
     
-    # Process your data here
+    data_list = list(data.values())
+    
+    
+    data_list[5] = str(data_list[5]) 
+    data_list[5].removeprefix("{")
+    data_list[5].removesuffix("}")
+    
+    print("Converted data to list:", data_list)
+    wb=load_workbook("C:/Users/szori/OneDrive/Asztali gép/Zalán mappa/Coding/Foldessportnap/Jelentkezők.xlsx")
+    print(wb.sheetnames)
+    ws=wb.active
+    ws.append(data_list)
+    wb.save("Jelentkezők.xlsx")
+    
     
     return jsonify({"status": "success", "message": "Data received"})
 
