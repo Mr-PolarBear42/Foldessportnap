c9c32f2511e826e3ad474c3e6472fcba491fe9c3
Szorenyi Zalan Andras <szorenyi.za@gmail.com>
2025-05-05 12:39:50 +0200

    asd

--- backend.py
+++ backend.py
@@ -47,5 +47,5 @@ def receive_data():
     
     return jsonify({"status": "success", "message": "Data received"})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
